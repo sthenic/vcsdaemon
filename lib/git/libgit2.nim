1b4eef3511c164fdcad886f27914d3a3f14fc06e
Marcus Eriksson <marcus.jr.eriksson@gmail.com>
2024-03-13 12:57:36 +0100

    Only support libgit2 v1.0 or later

--- lib/git/libgit2.nim
+++ lib/git/libgit2.nim
@@ -74,9 +74,6 @@ type
       push_update_reference*: pointer
       push_negotiation*: pointer
       transport*: pointer
-      # libgit2 v1.0.0 and v1.1.0 do not include the `remote_ready` member.
-      when not defined(libgit2_1v0_1v1):
-         remote_ready*: pointer
       payload*: pointer
       resolve_url*: pointer
 
